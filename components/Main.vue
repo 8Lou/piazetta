c798ff3d475a0c3eacf02b81b27d97377e142c8a
8Lou <lou8@mail.ru>
2024-07-11 01:46:08 +0300

    create-nuxt-app

---
+++ components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class='text-white'>УСЛУГИ</h1>
+        <button>Вызвать официанта</button>
+
+    <NavbarItem/>
+  </div>
+</template>
+
+<script>
+import NavbarItem from "./NavbarItem.vue";
+
+export default {
+  name: "Main",
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='services-container'>
+    <Main />
+      <ul class="list-none">
+        <li><nuxt-link to="/main">Главная</nuxt-link></li>
+        <li><nuxt-link to="/orders">Мои заказы</nuxt-link></li>
+      </ul>
+   
+  </div>
+</template>
+
+<script>
+import Main from "../components/Main.vue";
+
+export default {
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style scoped>
+.services-container {
+  width: 393px;
+  height: 852px;
+  background-color: #496172;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.list-none {
+  display: flex;
+  justify-content: space-between;
+  text-decoration: none; 
+  list-style-type: none;
+  
+  }
+</style>
